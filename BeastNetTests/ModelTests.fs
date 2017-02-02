838b58bc89d0f9b4c9fb4fe91a6826ce657b0f4d
Max Wilson <maxw@microsoft.com>
2017-02-02 10:33:28 -0800

    Imagination: can chain imaginations to read from real world without updating it.

--- BeastNetTests/ModelTests.fs
+++ BeastNetTests/ModelTests.fs
@@ -4,6 +4,7 @@ open Xunit
 open Util
 open Models
 open System
+open Stat
 
 [<Fact>]
 let CheckStatBonus() =
@@ -37,12 +38,47 @@ let TestScopes() =
     Stat.HP.Update 44 child2
     Assert.Equal(33, Stat.HP.Get child1)
     Assert.Equal(44, Stat.HP.Get child2)
-    Stat.HP.Set 4 glob
     let child1 = glob.spawn()
     let child2 = glob.spawn()
-    Assert.Equal(4, Stat.HP.Get child1) // Should have inherited value from global scope
+    Stat.HP.Set 4 glob
+    Assert.Equal(4, Stat.HP.Get child1) // Should have inherited value from global scope even though the scopes were created before global scope got value
     Assert.Equal(4, Stat.HP.Get child2)
     Stat.HP.Update 33 child1
     Stat.HP.Update 44 child2
     Assert.Equal(44, Stat.HP.Get child1) // Note: since this already existed at global scope, should update it at global scope, not child scope
     Assert.Equal(44, Stat.HP.Get child2)
+
+[<Fact>]
+let ``Imagination scopes should be able to read from but not write to real scopes``() =
+    let orc = Stat.monster("Orc", (16, 12, 14, 7, 10, 8, 15))
+    let customOrc = orc.spawn() // user-level customizations
+    Stat.Dex.Set 14 customOrc
+    Stat.Name.Set "Scro" customOrc
+    let orc1 = customOrc.spawn()
+    Stat.Name.Set "Leader" orc1
+    Stat.HP.Set 28 orc1
+    let orc2 = customOrc.spawn()
+    let orc3 = customOrc.spawn()
+    let orc4 = customOrc.spawn()
+    let orcs = [orc1;orc2;orc3;orc4]
+    Assert.Equal<int list>([14;14;14;14], orcs |> List.map Dex.Get)
+    Assert.Equal<int list>([28;15;15;15], orcs |> List.map HP.Get)
+    Assert.Equal<string list>(["Leader";"Scro";"Scro";"Scro"], orcs |> List.map Name.Get)
+    let imagined = orcs |> Stat.imagine
+    Stat.HP.Update 14 (imagined.[0])
+    Stat.Name.Update "Dugtash" (imagined.[1]) // imagine that the second orc's name is Dugtash
+    // check that changing imagination things works but has no impact on real things
+    Assert.Equal<int list>([14;15;15;15], imagined |> List.ofArray |> List.map HP.Get)
+    Assert.Equal<string list>(["Leader";"Dugtash";"Scro";"Scro"], imagined |> List.ofArray |> List.map Name.Get)
+    // but in the real world, HP and names haven't changed
+    Assert.Equal<int list>([28;15;15;15], orcs |> List.map HP.Get)
+    Assert.Equal<string list>(["Leader";"Scro";"Scro";"Scro"], orcs |> List.map Name.Get)
+    let nestedImagination = imagined |> Stat.imagine
+    // check that a nested imagination can still get values from the real world, and set values without disturbing our parent imagination
+    Stat.HP.Update 3 (nestedImagination.[1])
+    Assert.Equal<int list>([14;3;15;15], nestedImagination |> List.ofArray |> List.map Stat.HP.Get)
+    Assert.Equal<string list>(["Leader";"Dugtash";"Scro";"Scro"], nestedImagination |> List.ofArray |> List.map Name.Get)
+    Assert.Equal<int list>([28;15;15;15], orcs |> List.map HP.Get)
+    Assert.Equal<string list>(["Leader";"Scro";"Scro";"Scro"], orcs |> List.map Name.Get)
+
+
--- source/Models.fs
+++ source/Models.fs
@@ -16,20 +16,35 @@ type DieRoll = { N: int; DieSize: int; Plus: int } with
 module Stat =
     open System.Collections.Generic
 
-    type StatScope private (parentScope: StatScope option) =
+
+    type StatScope private (parentScope: StatScope option, ?shadowedScope: StatScope) =
         let vals = Dictionary<string, obj>()
+        let shadowRead (d1: IDictionary<'t1, 't2>) (d2 : IDictionary<'t1, 't2>) key =
+            match d1.TryGetValue key with
+            | true, v -> Some v
+            | _ ->
+                match d2.TryGetValue key with
+                | true, v -> Some v
+                | _ -> None
         new() = StatScope(None)
-        member this.get<'a>(key:string): 'a option =
+        member private this.shadowGet key =
             match vals.TryGetValue key with
-            | true, v -> Some(unbox v)
+            | true, v -> Some v
+            | _ ->
+                match shadowedScope with
+                | Some shadowed -> shadowed.shadowGet key
+                | None -> None
+        member this.get<'a>(key:string): 'a option =
+            match this.shadowGet key with
+            | Some v -> Some(unbox v)
             | _ ->
                 if parentScope.IsSome then
                     parentScope.Value.get key
                 else
                     None
         member this.update<'a>(key, (valueMaker: 'a -> 'a)) =
-            match vals.TryGetValue key with
-            | true, v ->
+            match this.shadowGet key with
+            | Some(v) ->
                 vals.[key] <- valueMaker(unbox v)
             | _ ->
                 if parentScope.IsSome then
@@ -40,8 +55,18 @@ module Stat =
             vals.[key] <- value
         member this.spawn() =
             StatScope(Some this)
-        member this.imagine(?realParent) =
-            ()
+        member this.imagine(context: Dictionary<StatScope, StatScope>) : StatScope =
+            match context.TryGetValue this with
+            | true, v -> v // if we're already imagined in this context (e.g. by multiple children), return cached imagination
+            | _ ->
+                match parentScope with
+                | Some parent -> StatScope(Some (parentScope.Value.imagine(context)), this)
+                | None ->
+                    StatScope(None, this)
+
+    let imagine (stats: StatScope seq) =
+        let ctx = Dictionary<StatScope, StatScope>()
+        stats |> Seq.map (fun x -> x.imagine(ctx)) |> Array.ofSeq
 
     type Property<'a> = { Name: string; Get: StatScope -> 'a; Update: 'a -> StatScope -> unit; Set: 'a -> StatScope -> unit }
     let valueProp<'a> name defaultVal =
@@ -55,4 +80,23 @@ module Stat =
 
     let HP = intProp "HP" 0
     let Name = stringProp "Name" "Nameless"
+    let Str = intProp "Str" 10
+    let Dex = intProp "Dex" 10
+    let Con = intProp "Con" 10
+    let Int = intProp "Int" 10
+    let Wis = intProp "Wis" 10
+    let Cha = intProp "Cha" 10
 
+    let monster(name, (str, dex, con, int, wis, cha, hp)) =
+        let sc = StatScope()
+        [
+            Str.Set str
+            Dex.Set dex
+            Con.Set con
+            Int.Set int
+            Wis.Set wis
+            Cha.Set cha
+            HP.Set hp
+            Name.Set name
+        ] |> List.iter (apply sc)
+        sc
