5843e57176f96ba05f57f5d34b504cca231338c1
Max Wilson <maxw@microsoft.com>
2017-02-03 22:19:35 -0800

    Refactor slightly

--- source/Beast.fs
+++ source/Beast.fs
@@ -7,15 +7,26 @@ module R = Fable.Helpers.React
 
 // Check components.fs to see how to build React components from F#
 open Components
+open Fable.Import.React
 
 // Polyfill for ES6 features in old browsers
 Node.require.Invoke("core-js") |> ignore
+
+open Util
+open Models
+open System
+open Stat
+
+type BeastUI() =
+    inherit Component<obj, obj>()
+    member this.render() =
+        R.div [] (this.children |> List.ofArray)
 
 ReactDom.render(
     R.div [] [
         R.h1 [] [R.str "BEAST 5E"]
         R.h3 [] [R.str "Brain-dead-simple Simulator Tool for 5E combats"]
-        R.text [] [R.str "placeholder"]
+        R.com<BeastUI,_,_> (obj()) [R.str "placeholder"]
         ],
     Browser.document.getElementById "content")
 |> ignore
